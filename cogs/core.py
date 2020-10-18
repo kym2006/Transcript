c56bfe1c6497edea8b997c890d9c8723b505e8b0
waterflamev8 <56286157+waterflamev8@users.noreply.github.com>
2020-10-18 16:51:10 +0800

    Update core.py

--- cogs/core.py
+++ cogs/core.py
@@ -13,7 +13,7 @@ class Core(commands.Cog):
     @commands.command(description="Exports channel history")
     async def export(self, ctx):
         with open(f"c{ctx.channel.id}.txt"):
-            await ctx.send("Here's the transcript!", file=discord.File(f"c{ctx.channel.id}.transcript"))
+            await ctx.send("Here's the transcript!", file=discord.File(f"c{ctx.channel.id}.txt"))
 
 
 def setup(bot):
